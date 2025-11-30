d47f5411717fd69670a1fb2aa4cec2b439dc33ae
justPriyanshuu <pmaurya.dev@gmail.com>
2025-11-30 13:13:26 +0530

    quick commit

---
+++ models/user.model.js
@@ -0,0 +1,16 @@
+import { integer, pgTable, varchar, uuid, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users', {
+  id: uuid().primaryKey().defaultRandom(),
+
+  first_name: varchar('first_name', { length: 50 }).notNull(),
+  last_name: varchar('last_name', { length: 50 }),
+
+  email: varchar({ length: 250 }).notNull().unique(),
+
+  password: text().notNull(),
+  salt: text().notNull(),
+
+  created_at: timestamp('created_at').defaultNow().notNull(),
+  updated_at: timestamp('updated_at').$onUpdate(() => new Date()),
+});
